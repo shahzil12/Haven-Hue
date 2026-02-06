341caa23708063323aed043d98876c676bae38bc
shahzil ahmed <shahzilahmedkhan8@gmail.com>
2026-02-06 21:48:58 +0500

    Configure Vercel PHP runtime

--- api/index.php
+++ api/index.php
@@ -1,7 +1,2 @@
 <?php
-// Force Debug Mode to see the error behind "500 | Server Error"
-putenv('APP_DEBUG=true');
-$_ENV['APP_DEBUG'] = 'true';
-$_SERVER['APP_DEBUG'] = 'true';
-
 require __DIR__ . '/../public/index.php';
